04ce0fb137e29f5a82115f5813aae96db7c2e27f
Kohei Morino <morino_kouhei@cyberagent.co.jp>
2016-01-30 23:25:25 +0900

    Fetch RSS

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 'use strict'
 
+let FeedParser = require('feedparser')
+let request = require('request-promise')
 let express = require('express')
 let app = express()
 
 app.get('/lm/news.json', (req, res) => {
-  let sample = ['this', 'is', 'a test!']
-  let data = {}
-  data.frames = sample.map((text, i) => {
-    return {
-      index: i,
-      text: text,
-      icon: 'a1278'
+  let feedParser = new FeedParser()
+  let num = 5
+  let items = []
+  request('https://english.kyodonews.jp/rss/news.xml').pipe(feedParser)
+  feedParser.on('readable', () => {
+    if (num-- <= 0) {
+      let data = {}
+      data.frames = items.map((item, i) => {
+        return {
+          index: i,
+          text: item.title,
+          icon: 'a1278'
+        }
+      })
+      res.json(data)
+      return
+    }
+    let item = feedParser.read()
+    if (item) {
+      items.push(item)
     }
   })
-  res.json(data)
 })
 
 app.listen(3000, () => {
